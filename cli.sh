a8e8cdc45124e5ea0e5b9018f2d18aced27cf315
David Golovan <david.glvn@gmail.com>
2019-11-17 16:52:46 +0200

    shorten long git branch names

--- cli.sh
+++ cli.sh
@@ -25,5 +25,11 @@ export KUBE_PS1_CLUSTER_FUNCTION=remove_username_from_cluster
 export KUBE_PS1_SYMBOL_ENABLE="false"
 source "/usr/local/opt/kube-ps1/share/kube-ps1.sh"
 
+# Git
+git_prompt=$(__git_ps1)
+if [ ${#git_prompt} -ge 5 ]; then 
+    git_prompt=${git_prompt::20}"...)"
+fi
+
 # The cli
-export PS1='\[$GREEN\]\t$(battery-status)$(ping-speed)\[$BLUE\][$(nvm current)]\[$RED\]-\[$BRIGHT_BLUE\]$WORKING_ENVIRONMENT\[$YELLOW\]\[$YELLOW\]$(~/bash_profile/scripts/short_pwd.py)\[\033[m\]\[$MAGENTA\]$(__git_ps1)\[$MAGENTA\]$(__trfrm_ps1)\[$WHITE\]$(kube_ps1)\$ '
+export PS1='\[$GREEN\]\t$(battery-status)$(ping-speed)\[$BLUE\][$(nvm current)]\[$RED\]-\[$BRIGHT_BLUE\]$WORKING_ENVIRONMENT\[$YELLOW\]\[$YELLOW\]$(~/bash_profile/scripts/short_pwd.py)\[\033[m\]\[$MAGENTA\]${git_prompt}\[$MAGENTA\]$(__trfrm_ps1)\[$WHITE\]$(kube_ps1)\$ '
